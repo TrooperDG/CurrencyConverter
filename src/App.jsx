c691713acff984f743ba58483d1266a8b6608889
Vishal Pandit <152159518+TrooperDG@users.noreply.github.com>
2025-02-08 21:12:54 +0530

    css and bugs

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,47 @@ function App() {
   const [convertedFrom, setConvertedFrom] = useState("usd");
   const [convertedTo, setConvertedTo] = useState("inr");
   const [convertedAmount, setConvertedAmount] = useState("");
+  const { data, fetchData } = useCurrencyInfo(convertedFrom);
+  // console.log(fetchCurrencyInfo);
 
-  const currencyInfo = useCurrencyInfo(convertedFrom);
-  const currencyOptions = Object.keys(currencyInfo);
+  let currencyInfo = data;
+  let currencyOptions = Object.keys(currencyInfo);
+  //! need to learn how to put the above lines in states
+  async function kipu() {
+    const newData = await fetchData(convertedTo);
+    currencyInfo = newData;
+    currencyOptions = Object.keys(currencyInfo);
+  }
 
   function swap() {
     setConvertedFrom(convertedTo);
     setConvertedTo(convertedFrom);
     setConvertedAmount(amount);
     setAmount(convertedAmount);
+    kipu();
   }
   function convert() {
     setConvertedAmount(Number(amount) * currencyInfo[convertedTo]);
+    // console.log(convertedTo);
     // console.log(currencyOptions);
   }
 
   return (
-    <div>
-      <div>
-        <div>
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('https://images.pexels.com/photos/6764526/pexels-photo-6764526.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+      }}
+    >
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-500 rounded-lg p-5 backdrop-blur-sm bg-white/30 ">
           <form
             onSubmit={(e) => {
               e.preventDefault();
               convert();
             }}
           >
-            <div>
+            <div className="w-full mb-1">
               <InputBox
                 label="From"
                 amount={amount}
@@ -42,12 +57,16 @@ function App() {
                 currencyOptions={currencyOptions}
               />
             </div>
-            <div>
-              <button type="button" onClick={swap}>
+            <div className=" relative w-full h-0.5 bg-slate-500">
+              <button
+                className="absolute bg-blue-600 left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md text-white px-2 py-0.5 "
+                type="button"
+                onClick={swap}
+              >
                 swap
               </button>
             </div>
-            <div>
+            <div className="w-full mt-1 mb-4 bg-orange-200">
               <InputBox
                 label="To"
                 amount={convertedAmount}
@@ -57,7 +76,10 @@ function App() {
                 amountDisable={true}
               />
             </div>
-            <button type="submit">
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+            >
               Convert {convertedFrom.toUpperCase()} to
               {convertedTo.toUpperCase()}
             </button>
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -3,17 +3,21 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
 
-  async function fetchData() {
+  async function fetchData(newCurrency) {
     const res = await fetch(
-      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${newCurrency}.json`
     );
     const data = await res.json();
     setData(data[currency]);
+    return data;
   }
 
-  useEffect(() => fetchData, [currency]);
+  useEffect(() => {
+    fetchData(currency);
+  }, [currency]);
   // console.log(data);
-  return data;
+  //* returning the funtion so that it can be called inside a funtion
+  return { data, fetchData };
 }
 
 export default useCurrencyInfo;
